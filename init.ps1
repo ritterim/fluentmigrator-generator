7e27fabb9376eb867681d720261d34c5ab44b828
Justin Rusbatch <jrusbatch@gmail.com>
2016-02-18 15:32:50 -0500

    Add initialization scripts

---
+++ init.ps1
@@ -0,0 +1,8 @@
+param($installPath, $toolsPath, $package)
+
+if (Get-Module | ?{ $_.Name -eq "FluentMigrator" })
+{
+    Remove-Module FluentMigrator
+}
+
+Import-Module (Join-Path $toolsPath "FluentMigrator.psd1
